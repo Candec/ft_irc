f5048a80365b7c182fe031c5e66999b1a4626dea
jcandec <omnius.candec@gmail.com>
2023-09-05 15:34:24 +0100

    defined channel hpp future methods, getters and setters

--- Server/includes/channel.hpp
+++ Server/includes/channel.hpp
@@ -1,12 +1,57 @@
 #ifndef CHANNEL_HPP
 # define CHANNEL_HPP
 
+# include "user.hpp"
+
+# include <string>
+# include <vector>
+# include <map>
+
 class Channel
 {
 	public:
+		channel();
+		~channel();
+
+		// void setName(std::string name);
+		// std::string getName();
+
+		// void setMode(std::string Mode);
+		// std::string getMode();
+
+		// void setDescription(std::string Description);
+		// std::string getDescription();
+
+		// void setkey(std::string key);
+		// std::string getkey();
+
+		// void addUser(User &user);
+		// void removeUser(User &user);
+		// void removeUser(const std::string &nick);
+		// std::vector<User *> getUsers();
+
+		// bool isUser(User &user);
+		// bool isOnChannel(User &user);
+
+		//void setUserMax(uint16_t max);
+
+		// void addInvitedUser(User &user);
+		// void isInvitedUser(User &user);
+		// void revokeInvitation(User &user);
+
+		//void brodcast(User &user, std::string brodcast);
 
 	private:
+		std::string name;
+		std::string mode;
+		std::string description;
+		std::string key;
 
+		std::map<int, User *> users;
+		std::map<int, User *> user_mode;
+		uint16_t user_max;
+
+		std::vector<User *> invitations;
 }
 
 #endif
