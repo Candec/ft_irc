51233ca68fb8d88b7133d5fa6a161e9749d656fd
jcandec <omnius.candec@gmail.com>
2023-09-14 15:26:52 +0100

    added config file and slighly touched the main

--- srcs/ircserv.cpp
+++ srcs/ircserv.cpp
@@ -1,25 +1,17 @@
 #include "server.hpp"
+#include "utility.hpp"
 
 int main(int argc, char*argv[])
 {
 	if (argc != 3)
-	{
-		std::cout << "Error" << std::endl;
-		std::cout << "./ircserv <port> <password>" << std::endl;
-		return (1);
-	}
-
-	if (atoi(argv[1]) <= 0 || atoi(argv[1]) > 49150)
-	{
-		std::cout << "Error" << std::endl;
-		std::cout << "Port out of range" << std::endl;
-		return (1);
-	}
+		error("./ircserv <port> <password>", EXIT);
 
 	Server server(argv[1], argv[2]);
 
 	while (server.run)
 		server.start();
 	return (0);
+	
+
 
 }
